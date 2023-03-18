c30e64e50417dd6d5f10d8deba0976c604233cee
beer <beer5214@126.com>
2023-03-19 00:01:18 +0800

    update test case

--- list/list_test.go
+++ list/list_test.go
@@ -8,6 +8,10 @@ type People struct {
 	name string
 }
 
+type NewType[T int | *People] struct {
+	value T
+}
+
 func TestName(t *testing.T) {
 	l := New[*People]()
 	l.PushFront(&People{
@@ -18,4 +22,17 @@ func TestName(t *testing.T) {
 	p = l.Back().Value
 
 	t.Log(p.name)
+
+	l2 := New[*NewType[int]]()
+
+	l2.PushBack(&NewType[int]{
+		value: 200,
+	})
+	l2.PushBack(&NewType[int]{
+		value: 100,
+	})
+	t.Log(l2.Len())
+	t.Log(l2.Back().Value) // 100
+	l2.Remove(l2.Back())
+	t.Log(l2.Back().Value) //200
 }
